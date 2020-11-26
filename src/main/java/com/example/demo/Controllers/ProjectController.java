78b5638a36c7a6a22ff22d59f5692e661b52a249
peter <peter.lm.schmidt@gmail.com>
2020-11-26 17:00:12 +0100

    små rettelser

--- src/main/java/com/example/demo/Controllers/ProjectController.java
+++ src/main/java/com/example/demo/Controllers/ProjectController.java
@@ -8,10 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
 
 @Controller
 public class ProjectController {
