54ee7937ef224209941080813968b3be2a146ac8
AdamHviid <adam5596@stud.kea.dk>
2020-11-26 12:03:14 +0100

    adam, added project list

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -1,15 +1,41 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Models.Project;
+import com.example.demo.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
+    UserRepository ur = new UserRepository();
+    List<Project> projectList = new ArrayList<>();
+
 
     @GetMapping("/main")
-    public String main(){
+    public String main(Model m) throws SQLException {
+        projectList = ur.getUsersProjects(1);
+        System.out.println("number of projects: " + projectList.size());
+        m.addAttribute("projectList",projectList);
         return "main";
     }
 
+    @PostMapping("/backtomain")
+    public String backToMain() {
+        projectList.clear();
+        return "redirect:/main";
+    }
+
+    @PostMapping("/project")
+    public String project() {
+        return "project";
+    }
+
+    //@PostMapping("/projects")
     //controller klik på/vis projekt
 }
