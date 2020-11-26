60f31d67c96bb9ada1614a481c99b891fdd3fa99
AdamHviid <adam5596@stud.kea.dk>
2020-11-26 14:33:23 +0100

    adam, added create project method

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -1,11 +1,13 @@
 package com.example.demo.Controllers;
 
 import com.example.demo.Models.Project;
+import com.example.demo.Repositories.ProjectRepository;
 import com.example.demo.Repositories.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -14,11 +16,13 @@ import java.util.List;
 @Controller
 public class MainController {
     UserRepository ur = new UserRepository();
+    ProjectRepository pr = new ProjectRepository();
     List<Project> projectList = new ArrayList<>();
 
 
     @GetMapping("/main")
     public String main(Model m) throws SQLException {
+        projectList.clear();
         projectList = ur.getUsersProjects(1);
         System.out.println("number of projects: " + projectList.size());
         m.addAttribute("projectList",projectList);
@@ -34,6 +38,23 @@ public class MainController {
     @PostMapping("/project")
     public String project() {
         return "project";
+    }
+
+    @PostMapping("/createproject")
+    public String createProject(WebRequest wr) throws SQLException {
+        String projectName = wr.getParameter("projectName");
+
+        //dette giver dagens dato i yyyy/mm/dd
+        long millis=System.currentTimeMillis();
+        java.sql.Date currentDay = new java.sql.Date(millis);
+
+        //ændre til currentlogins id, når den er klar :)
+        int createdBy = 1;
+
+        pr.createProject(projectName, currentDay, createdBy);
+        return "redirect:/main";
+
+
     }
 
     //@PostMapping("/projects")
--- src/main/java/com/example/demo/Repositories/ProjectRepository.java
+++ src/main/java/com/example/demo/Repositories/ProjectRepository.java
@@ -1,4 +1,23 @@
 package com.example.demo.Repositories;
 
+import com.example.demo.Services.DBConnect;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+@Repository
 public class ProjectRepository {
+    DBConnect connection = new DBConnect();
+
+    public void createProject(String projectName, Date createdDate, int createdBy) throws SQLException {
+        PreparedStatement ps = connection.establishConnection().prepareStatement("INSERT INTO projects (project_name, project_created_date, created_by) VALUES (?,?,?)");
+        ps.setString(1,projectName);
+        ps.setDate(2,createdDate);
+        ps.setInt(3,createdBy);
+
+        ps.executeUpdate();
+    }
 }
