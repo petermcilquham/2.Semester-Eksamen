c26fb11fb5407dbc7c2b97002a6391b6cb392eaf
Anton <anton.riis@gmail.com>
2020-11-27 14:25:48 +0100

    Jeg er begyndt på Tasks

--- src/main/java/com/example/demo/Repositories/TaskRepository.java
+++ src/main/java/com/example/demo/Repositories/TaskRepository.java
@@ -11,17 +11,18 @@ import java.util.GregorianCalendar;
 public class TaskRepository {
     DBConnect connection = new DBConnect();
 
-    public void createTask(String taskName, Date currentDay, Date endDate) throws SQLException {
-        PreparedStatement ps = connection.establishConnection().prepareStatement("INSERT INTO tasks (taskName,startDate,endDate) VALUES (?,?,?)");
+    public void createTask(String taskName, Date currentDay, String endDate, int projectID) throws SQLException {
+        PreparedStatement ps = connection.establishConnection().prepareStatement("INSERT INTO tasks (task_name,start_date,end_date,projectID) VALUES (?,?,?,?)");
         ps.setString(1,taskName);
         ps.setDate(2, currentDay);
-        ps.setDate(3, endDate);
+        ps.setString(3, endDate);
+        ps.setInt(4, projectID);
 
         ps.executeUpdate();
     }
 
     public void deleteTask(int id) throws SQLException {
-        PreparedStatement ps = connection.establishConnection().prepareStatement("DELETE FROM projects WHERE projectID = ?");
+        PreparedStatement ps = connection.establishConnection().prepareStatement("DELETE FROM tasks WHERE projectID = ?");
         ps.setInt(1,id);
         ps.executeUpdate();
     }
