7164aa5c6985e8fb7f65be925d446a888b7794db
Adam <adam5596@stud.kea.dk>
2020-12-02 12:48:42 +0100

    adam, edit table ting :)

--- src/main/java/com/example/demo/Controllers/MainController.java
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
 @Controller
--- src/main/java/com/example/demo/Controllers/UserController.java
+++ src/main/java/com/example/demo/Controllers/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 
 @Controller
